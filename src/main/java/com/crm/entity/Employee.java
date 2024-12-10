2e791f665541035f91ce4199c6b1d64ac9e03a11
Apurv Soni <apurvs240@gmail.com>
2024-12-10 23:52:54 +0530

    test 1

---
+++ src/main/java/com/crm/entity/Employee.java
@@ -0,0 +1,42 @@
+package com.crm.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+//import org.hibernate.proxy.HibernateProxy;
+
+//import java.util.Objects;
+
+@Data
+@Entity
+@Table(name = "employee")
+public class Employee {
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "email_id", nullable = false, unique = true)
+    private String emailId;
+
+    @Column(name = "mobile", nullable = false, unique = true, length = 10)
+    private String mobile;
+
+//    @Override
+//    public final boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null) return false;
+//        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+//        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+//        if (thisEffectiveClass != oEffectiveClass) return false;
+//        Employee employee = (Employee) o;
+//        return id != null && Objects.equals(id, employee.id);
+//    }
+//
+//    @Override
+//    public final int hashCode() {
+//        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+//    }
+}
